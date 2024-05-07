0d9d1bd6ee80047e123b92c2c5c327d41ae108e2
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-05-06 19:48:53 -0500

    working on the animation frames

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ $(document).ready(function () {
   
 
   const position = {
-    x: 0,
-    y: 0,
+    x: GameViewport.WIDTH / 2 - jin[0].width / 2,
+    y: 100,
   };
 
   let velocity = 1;
@@ -30,16 +30,20 @@ $(document).ready(function () {
     }
 
     // context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
-    context.drawImage(background[0], 0, 0);
+    context.drawImage(background[0], 0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
 
-    context.strokeStyle = "yellow";
-    context.moveTo(0, 0);
-    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-    context.moveTo(GameViewport.WIDTH, 0);
-    context.lineTo(0, GameViewport.HEIGHT);
-    context.stroke();
+    // context.strokeStyle = "yellow";
+    // context.moveTo(0, 0);
+    // context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+    // context.moveTo(GameViewport.WIDTH, 0);
+    // context.lineTo(0, GameViewport.HEIGHT);
+    // context.stroke();
 
-    context.drawImage(jin[0], position.x, position.y);
+    const jinWidth = jin[0].width * 0.8;
+    const jinHeight = jin[0].height * 0.8;
+    context.drawImage(jin[0], position.x, position.y, jinWidth, jinHeight);
+
+    // context.drawImage(jin[0], position.x, position.y);
 
     window.requestAnimationFrame(frame);
   }
