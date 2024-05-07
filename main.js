025e51117f9c30a0b8a7e9ec80423c5fb54377b1
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-05-06 19:15:21 -0500

    working on background

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
-$(document).ready(function() {
-    const GameViewport = {
-        WIDTH: 384,
-        HEIGHT: 224,
-    };
+$(document).ready(function () {
+  const GameViewport = {
+    WIDTH: 384,
+    HEIGHT: 224,
+  };
 
-    const canvasEl = $('canvas'); 
-    const canvasDomEl = canvasEl[0]; 
-    const context = canvasDomEl.getContext('2d');
+  const canvasEl = $("canvas");
+  const canvasDomEl = canvasEl[0];
+  const context = canvasDomEl.getContext("2d");
 
-    canvasDomEl.width = GameViewport.WIDTH;
-    canvasDomEl.height = GameViewport.HEIGHT;
+  canvasDomEl.width = GameViewport.WIDTH;
+  canvasDomEl.height = GameViewport.HEIGHT;
 
-    const jin = $('img');
+  const jin = $('img').eq(0);
+  const background = $('img').eq(1);
+  
 
-    context.strokeStyle = 'yellow';
+  const position = {
+    x: 0,
+    y: 0,
+  };
+
+  let velocity = 1;
+
+  function frame() {
+    position.x += velocity;
+
+    if (position.x > GameViewport.WIDTH - jin[0].width || position.x < 0) {
+      velocity *= -1;
+    }
+
+    // context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+    context.drawImage(background[0], 0, 0);
+
+    context.strokeStyle = "yellow";
     context.moveTo(0, 0);
     context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
     context.moveTo(GameViewport.WIDTH, 0);
     context.lineTo(0, GameViewport.HEIGHT);
     context.stroke();
 
-    context.drawImage(jin[0], 0, 0);
+    context.drawImage(jin[0], position.x, position.y);
 
-    console.log(context);
+    window.requestAnimationFrame(frame);
+  }
+
+  window.requestAnimationFrame(frame);
+
+//   console.log(context);
 });
